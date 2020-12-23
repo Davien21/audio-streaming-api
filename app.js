d7bee4cc5cd628600a0bed37ab0581615aa7b9df
davien21 <chidiebereekennia@gmail.com>
2020-12-23 14:00:06 +0100

    add simple GET route to test app

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.get("/api/audio", (req, res) => {
+app.get("/api/audio/", (req, res) => {
   res.send("Hi, audio isn't ready yet 😉");
 });
- 
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
