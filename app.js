75172623d5b0177907387e4a3285921143d0d244
davien21 <chidiebereekennia@gmail.com>
2020-12-23 14:04:16 +0100

    send audio in /audio route for testing

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/api/audio/", (req, res) => {
-  res.send("Hi, audio isn't ready yet 😉");
+  res.sendFile("audio/24kGoldn - Mood ft. Iann Dior.mp3", { root: __dirname });
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
