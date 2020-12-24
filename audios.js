da2d15b58effb09673c826af9e5e5c0349f5af1a
davien21 <chidiebereekennia@gmail.com>
2020-12-24 01:41:35 +0100

    add new audio for testing

--- audios.js
+++ audios.js
@@ -1,6 +1,7 @@
 const audioData = [
   { id: 0, title: "24kGoldn - Mood ft. Iann Dior.mp3" },
   { id: 1, title: "Rema - Peace of Mind.mp3" },
+  { id: 2, title: "Chief_Stephen_Osita_Osadebe_-_Ana_Amasi_ife.mp3" },
 ];
 
-module.exports = audioData
+module.exports = audioData;
